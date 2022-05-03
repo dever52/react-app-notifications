92aa8142a582c0d7dbe99bb84ab4446870d1cee1
dever52 <58336610+dever52@users.noreply.github.com>
2022-05-03 22:37:25 +0200

    Icon update

--- src/icons/CircleExclamation.tsx
+++ src/icons/CircleExclamation.tsx
@@ -2,9 +2,45 @@ import * as React from 'react';
 
 const CircleExclamation = (props: any) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
-            <path
-                d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256 256-114.6 256-256S397.4 0 256 0zm-24 152c0-13.2 10.8-24 24-24s24 10.75 24 24v128c0 13.25-10.75 24-24 24s-24-10.7-24-24V152zm24 248c-17.36 0-31.44-14.08-31.44-31.44s14.07-31.44 31.44-31.44 31.44 14.08 31.44 31.44C287.4 385.9 273.4 400 256 400z"/>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 909.33 909.33"
+            {...props}
+        >
+            <circle
+                cx={454.67}
+                cy={454.67}
+                r={454.17}
+                style={{
+                    stroke: "#1d1d1b",
+                    strokeMiterlimit: 10,
+                }}
+                data-name="Ebene 2"
+            />
+            <g data-name="Ebene 3">
+                <circle
+                    cx={454.67}
+                    cy={741.33}
+                    r={85.94}
+                    style={{
+                        fill: "#fff",
+                        stroke: "#1d1d1b",
+                        strokeMiterlimit: 10,
+                    }}
+                />
+                <rect
+                    x={368.72}
+                    y={82.06}
+                    width={171.89}
+                    height={512}
+                    rx={85.94}
+                    style={{
+                        fill: "#fff",
+                        stroke: "#1d1d1b",
+                        strokeMiterlimit: 10,
+                    }}
+                />
+            </g>
         </svg>
     );
 };
